98845cfdc4b4d70db7a4b64654e2eb9ff7ecf545
quantike <55246750+quantike@users.noreply.github.com>
2025-02-28 20:43:33 -0600

    feat: matching engine

---
+++ app/dependencies.py
@@ -0,0 +1,38 @@
+# The database
+from dataclasses import dataclass
+from typing import Dict, List
+
+
+@dataclass
+class Beta:
+    prompt: str
+
+
+@dataclass
+class Chad:
+    id: str
+    tier: int
+    prompt: str
+
+
+class MatchingEngine:
+
+    def __init__(self) -> None:
+        self.chads: List[Chad] = []
+        self.betas: List[Beta] = []
+
+    def match(self) -> Dict[str, str | None] | None:
+        # Ensure there is at least one element in each list
+        if not self.betas:
+            return None
+
+        user_prompt = self.betas.pop(0).prompt
+        system_prompt = self.chads.pop(0).prompt if self.chads else None
+        
+        return {
+            # This will never be None
+            "user_prompt": user_prompt,
+
+            # This can be None
+            "system_prompt": system_prompt,
+        }
--- app/routers/chadvertisers.py
+++ app/routers/chadvertisers.py
@@ -1,4 +1,7 @@
+import uuid
+
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix="/chadvertisers",
@@ -6,6 +9,18 @@ router = APIRouter(
     responses={404: {"description":  "not found"}}
 )
 
+# TODO: Store the UUIDs somewhere
+@router.post("/signup")
+async def post_login():
+    return uuid.uuid4()
 
+
+class CampaignPayload(BaseModel):
+    id: str
+    prompt: str
+
+
+# TODO: Check that the ID exists somehere
 @router.post("/campaign")
-async def post_campaign(req: ):
+async def post_campaign(payload: CampaignPayload):
+    return payload.prompt
