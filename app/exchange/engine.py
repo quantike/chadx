c605acd90cb80b3942c8d54efcfb5101c22e00dd
quantike <55246750+quantike@users.noreply.github.com>
2025-03-01 00:47:27 -0600

    feat: similarity matching

--- app/exchange/engine.py
+++ app/exchange/engine.py
@@ -1,27 +1,43 @@
-from typing import Dict, List
-
+import numpy as np
 from loguru import logger
+
+from app.exchange.embedder import Embedder
 
 from .models import Beta, Chad
 
 
 class MatchingEngine:
     """
-    The matching engine takes in a campaign and serves it based on a relevance threshold.
+    The matching engine takes in a campaign and matches the copy to incoming prompts.
     """
 
-    def __init__(self) -> None:
-        self.chads: List[Chad] = []
+    def __init__(self, chad: Chad) -> None:
+        self.chad = chad
+        self.embedder = Embedder()
+        self.chad_embedding = self.embedder.embed([self.chad.copy])[0]
 
-    async def match(self, beta: Beta) -> Dict[str, str | None] | None:
-        user_prompt = beta.prompt
-        system_prompt = self.chads.pop(0).copy if self.chads else None
-        logger.info("match")
-        
-        return {
-            # This will never be None
-            "user_prompt": user_prompt,
+    async def match(self, beta: Beta):
+        """
+        Matching algorithm:
 
-            # This can be None
-            "system_prompt": system_prompt,
-        }
+        - Embed a beta prompt.
+        - If the cosine similarity exceeds the campaign threshold, inject it as a system_prompt
+        - If below threshold, no match it made, but fulfill request anyway.
+        """
+        beta_embedding = self.embedder.embed([beta.prompt])[0]
+
+        similarity = np.dot(beta_embedding, self.chad_embedding) / (
+            np.linalg.norm(beta_embedding) * np.linalg.norm(self.chad_embedding)
+        )
+        logger.info(similarity)
+
+        if similarity >= self.chad.threshold:
+            return {
+                "beta": beta,
+                "chad": self.chad,
+            }
+        else:
+            return {
+                "beta": beta,
+                "chad": None,
+            }
